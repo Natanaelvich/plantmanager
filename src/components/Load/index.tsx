c855142a77aec466cfe7270329b93e69cecbb79b
Natanael Lima <taelima1997@gmail.com>
2021-04-21 18:11:53 -0300

    add load component

---
+++ src/components/Load/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import LottieView from 'lottie-react-native';
+
+import loadAnimation from '../../assets/load.json';
+
+import { Container } from './styles';
+
+const Load: React.FC = () => {
+  return (
+    <Container>
+      <LottieView
+        source={loadAnimation}
+        autoPlay
+        loop
+        style={{
+          backgroundColor: 'transparent',
+          width: 200,
+          height: 200,
+        }}
+      />
+    </Container>
+  );
+};
+
+export default Load;
