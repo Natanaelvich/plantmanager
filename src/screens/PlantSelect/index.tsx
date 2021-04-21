d524ac96a889d29d750d2862a5f09f5f7937bcca
Natanael Lima <taelima1997@gmail.com>
2021-04-21 17:47:42 -0300

    add screen plant select

---
+++ src/screens/PlantSelect/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+import { Container } from './styles';
+
+const PlantSelect: React.FC = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default PlantSelect;
---
+++ src/screens/PlantSelect/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background: ${colors.background};
+`;
