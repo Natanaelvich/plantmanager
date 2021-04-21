65d9ac637895bf53ba2af10864e661d47e0ab0d4
Natanael Lima <taelima1997@gmail.com>
2021-04-21 17:49:18 -0300

    update plant select

--- src/screens/PlantSelect/index.tsx
+++ src/screens/PlantSelect/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import EnviromentButton from '../../components/EnviromentButton';
 import Header from '../../components/Header';
-import { Container } from './styles';
+import {
+  Container,
+  HeaderContent,
+  Title,
+  Subtitle,
+  FlatListContainer,
+  ListItem,
+} from './styles';
 
 const PlantSelect: React.FC = () => {
   return (
     <Container>
-      <Header />
+      <HeaderContent>
+        <Header />
+
+        <Title>Em qual ambiente</Title>
+        <Subtitle>você quer colocar sua planta?</Subtitle>
+      </HeaderContent>
+
+      <FlatListContainer>
+        <ListItem
+          data={[1, 2, 3, 4, 5]}
+          renderItem={({ item }) => <EnviromentButton title="Cozinha" />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </FlatListContainer>
     </Container>
   );
 };
--- src/screens/PlantSelect/styles.ts
+++ src/screens/PlantSelect/styles.ts
@@ -1,7 +1,39 @@
 import styled from 'styled-components/native';
 import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   background: ${colors.background};
 `;
+
+export const HeaderContent = styled.View`
+  padding: 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 17px;
+  color: ${colors.heading};
+  font-family: ${fonts.heading};
+  line-height: 20px;
+  margin-top: 15px;
+`;
+
+export const Subtitle = styled.Text`
+  font-family: ${fonts.text};
+  font-size: 17px;
+  line-height: 20px;
+  color: ${colors.heading};
+`;
+
+export const FlatListContainer = styled.View``;
+
+export const ListItem = styled.FlatList.attrs({
+  contentContainerStyle: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
+  },
+})``;
