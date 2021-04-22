53f927edc8ebc1adc5b7eeff3c09d7a0799c623b
Natanael Lima <taelima1997@gmail.com>
2021-04-22 20:50:29 -0300

    add bottom tabs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,8 @@ import Welcome from '../screens/Welcome';
 import UserIdentification from '../screens/UserIdentification';
 import colors from '../styles/colors';
 import Confirmation from '../screens/Confirmation';
-import PlantSelect from '../screens/PlantSelect';
 import PlantSave from '../screens/PlantSave';
-import MyPlants from '../screens/MyPlants';
+import AuthRoutes from './tab.routes';
 
 const StackRoutes = createStackNavigator();
 
@@ -30,11 +29,11 @@ const Routes: React.FC = () => (
 
       <StackRoutes.Screen name="Confirmation" component={Confirmation} />
 
-      <StackRoutes.Screen name="PlantSelect" component={PlantSelect} />
+      <StackRoutes.Screen name="PlantSelect" component={AuthRoutes} />
 
       <StackRoutes.Screen name="PlantSave" component={PlantSave} />
 
-      <StackRoutes.Screen name="MyPlants" component={MyPlants} />
+      <StackRoutes.Screen name="MyPlants" component={AuthRoutes} />
     </StackRoutes.Navigator>
   </NavigationContainer>
 );
