2ea33acfdd6954418df03e3e67a10abada16a3d9
Natanael Lima <taelima1997@gmail.com>
2021-04-22 20:25:37 -0300

    update header with user storage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Container, Content, Greeting, UserName, Avatar } from './styles';
 
 const Header: React.FC = () => {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    async function loadStorageUserName(): Promise<void> {
+      const user = await AsyncStorage.getItem('@plantmanager:user');
+      setUserName(user || '');
+    }
+
+    loadStorageUserName();
+  }, []);
+
   return (
     <Container>
       <Content>
         <Greeting>Olá,</Greeting>
-        <UserName>Natanael</UserName>
+        <UserName>{userName}</UserName>
       </Content>
 
       <Avatar source={{ uri: 'https://github.com/Natanaelvich.png' }} />
