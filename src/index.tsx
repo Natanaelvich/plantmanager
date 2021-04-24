0d73f13c6412bc6f987cc1287cc6a7bce320ab94
Natanael Lima <taelima1997@gmail.com>
2021-04-23 21:28:49 -0300

    add setNotificationHandler

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
 } from '@expo-google-fonts/jost';
 import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
+
 import Routes from './routes';
 
 const Main: React.FC = () => {
@@ -12,6 +14,25 @@ const Main: React.FC = () => {
     Jost_400Regular,
     Jost_600SemiBold,
   });
+
+  useEffect(() => {
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async notification => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log(data);
+    //   },
+    // );
+
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
+    // return () => subscription.remove();
+  }, []);
 
   if (!fontsLoaded) return <AppLoading />;
 
